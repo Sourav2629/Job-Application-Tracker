785e1add24644215b82e71bee8e713dad1414932
Sourav Kumar <sourav.keshri.32@gmail.com>
2026-01-29 23:52:44 +0530

    Reorder import statements in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
+import './utils/axiosConfig';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './utils/axiosConfig';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(<App />); 
+
