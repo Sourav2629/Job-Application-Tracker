745d47d15ee882c84bd4bb546d22b2127a8b092f
Sourav Kumar <sourav.keshri.32@gmail.com>
2026-01-30 15:53:17 +0530

    Refactor import statements and rendering code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,7 @@
-import './utils/axiosConfig';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-
 const container = document.getElementById('root');
 const root = createRoot(container);
-
-root.render(<App />); 
-
+root.render(<App />);
